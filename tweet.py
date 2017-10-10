875ff928f5a40b9a30590f49911867e6f7f0874f
David Vargas <dvcarrillo@correo.ugr.es>
2017-10-10 11:59:33 +0200

    Now it handles errors/abortions and tweet if something goes wrong

--- tweet.py
+++ tweet.py
@@ -9,6 +9,7 @@
 import tweepy
 import datetime
 import time
+import sys
 
 # Log in to Twitter
 auth = tweepy.OAuthHandler('consumer_key',
@@ -18,24 +19,38 @@ auth.set_access_token('access_key',
 
 # Get the API variable
 api = tweepy.API(auth)
+print '#WhatHappensInMyNeighborhood'
 
 # Get the current time
 actual_time = datetime.datetime.utcnow()
 print 'Actual UTC time is ' + str(actual_time)
 
-while True:
-    time.sleep(10)
-    # Get the last mention and its time and ID
-    mentions = api.mentions_timeline(count=1)
-    mention_user = mentions[0].user.screen_name
-    mention_time = mentions[0].created_at
-    mention_id = mentions[0].id
+# Start hearing mentions
+print '\nHearing mentions now...'
+start_status = api.update_status('#whimn hearing mentions now...')
 
-    # If the account is mentioned after the script is launched, it sends a tweet
-    if mention_time >= actual_time:
-        text = '@' + str(mention_user) + ' Cats make people happy!'
-        new_status = api.update_with_media("./random_cat.jpeg", text, in_reply_to_status_id = mention_id)
-        actual_time = datetime.datetime.utcnow()
-        print 'A new tweet has been sent!'
-    else:
-        print 'Nothing new after 10s'
+# Infinite loop that reads mentions every 10s
+while True:
+    try:
+        # Wait 10s
+        time.sleep(10)
+        # Get the last mention
+        mentions = api.mentions_timeline(count=1)
+        mention_user = mentions[0].user.screen_name
+        mention_time = mentions[0].created_at
+        mention_id = mentions[0].id
+        # If the account is mentioned after the script is launched, it sends a tweet
+        if mention_time >= actual_time:
+            text = '@' + str(mention_user) + ' Cats make people happy!'
+            photo_status = api.update_with_media("./random_cat.jpeg", text, 
+                in_reply_to_status_id = mention_id)
+            actual_time = datetime.datetime.utcnow()
+            print 'A new tweet has been sent! (' + str(datetime.datetime.utcnow()) +')'
+        else:
+            print 'Nothing new...'
+    # If something goes wrong, warn it and stop the execution
+    except:
+        api.destroy_status(start_status.id)
+        api.update_status('#whimn stopped')
+        print '\nStopping...'
+        sys.exit()
